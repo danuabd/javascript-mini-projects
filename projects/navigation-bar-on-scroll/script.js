e28fd08357f951f72f908e05553ca03a40891c0d
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2024-06-19 21:52:42 +0530

    Add "Navigation bar on scroll" project

---
+++ projects/navigation-bar-on-scroll/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const navbar = document.querySelector(".section--navbar");
+let oldScrollY = window.scrollY;
+let revealNavbar = false;
+
+// Reveal Sidebar on scroll
+const navbarObserver = new IntersectionObserver(function (e) {
+  const [observer] = e;
+  if (!observer.isIntersecting) {
+    console.log(observer);
+    revealNavbar = true;
+  }
+});
+navbarObserver.observe(navbar);
+
+window.addEventListener("scroll", function () {
+  //   Reset when scrolled up
+  if (Number(window.scrollY) < 200) {
+    revealNavbar = false;
+  } else {
+    revealNavbar = true;
+  }
+
+  // when scrolled down
+  if (oldScrollY < window.scrollY && revealNavbar) {
+    console.log(revealNavbar);
+    navbar.style.opacity = 0;
+  }
+
+  // when scrolled up
+  if (oldScrollY > window.scrollY && revealNavbar) {
+    console.log(revealNavbar);
+    navbar.classList.add("navbar--sticky");
+    navbar.style.opacity = 1;
+  }
+  oldScrollY = window.scrollY;
+});
