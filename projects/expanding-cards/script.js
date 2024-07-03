63a3dd3dce09eb2ba31f8a372e527d14dc6c42ef
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2024-07-03 12:04:43 +0530

    Add responsive styles to "Expanding Cards" project

--- projects/expanding-cards/script.js
+++ projects/expanding-cards/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const expandingCardsContainer = document.querySelector(".expanding-cards");
+const cards = document.querySelectorAll(".expanding__card");
+
+// Event handler for clicks
+expandingCardsContainer.addEventListener("click", function (e) {
+  if (!e.target.closest("IMG")) return;
+  console.log(e.target);
+
+  // remove expanded class from all image figure elements
+  cards.forEach((card) => card.classList.remove("card--expanded"));
+
+  // add expanded class to the selected image figure element
+  e.target.closest(".expanding__card").classList.add("card--expanded");
+});
