52f90c73cdd56e6afd41cba55adb6afe57c36acc
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2024-07-04 18:36:31 +0530

    Complete "Rotating Navigation" project

--- projects/rotating-navigation/script.js
+++ projects/rotating-navigation/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const postNavigation = document.querySelector(".navigation__toggle");
+const post = document.querySelector(".rotating__blog-post");
+const menu = document.querySelector(".navigation__menu");
+
+postNavigation.addEventListener("click", function (e) {
+  if (!e.target.closest("BUTTON")) return;
+
+  post.classList.toggle("rotating__blog-post--rotate");
+
+  menu.classList.toggle("navigation__menu--hidden");
+
+  postNavigation.classList.toggle("navigation__toggle--rotate");
+});
