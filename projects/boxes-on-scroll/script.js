d0eb949ad6f33c12f6250d3f0c0a50222eb8fa6b
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2024-07-05 19:57:36 +0530

    Add structure to the "Boxes On Scroll" project

--- projects/boxes-on-scroll/script.js
+++ projects/boxes-on-scroll/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const boxContainer = document.querySelector(".boxes");
+let boxes = document.getElementsByClassName("boxes__box");
+
+// create a box to be inserted
+const box = `<div class="boxes__box    
+            box--moving-right">
+              <h3 class="h3 margin-s">Lorem Ipsum</h3>
+              <p class="p">
+               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
+               commodi expedita facere consequuntur accusantium minima ea nulla
+               quas voluptatibus!
+              </p>
+            </div>`;
