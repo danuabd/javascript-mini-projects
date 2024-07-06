f5e7b84dc7b07b927b16f61085b85a22feb37b59
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2024-07-06 13:46:48 +0530

    Complete "Boxes on Scroll" project
    -Add scroll event
    -Add determinate function for scroll up and down
    -Add 2 observers, each for scroll up and down
    -Use "container.insertAdjacentHTML()", "container.removeChild()", and "element.classList.toggle()" methods to remove and add elements

--- projects/boxes-on-scroll/script.js
+++ projects/boxes-on-scroll/script.js
@@ -18,14 +18,76 @@ const box = `<div class="boxes__box
 let previous = 0;
 let present;
 
-// check if scrolling up or down
+// hold last box element and (lastBox-1) box element
+let lastBox, previousBox;
+
+// check if scrolling up or down (throttle this later)
 window.addEventListener("scroll", function (e) {
   present = document.documentElement.scrollTop.toFixed(0);
 
+  // hold difference
   const change = present - previous;
 
-  if (change > 0) console.log("down");
-  if (change < 0) console.log("up");
+  // get last box and previous box
+  lastBox = boxes[boxes.length - 1];
+  previousBox = boxes[boxes.length - 2];
 
+  // Logic when scrolling DOWN
+  if (change > 0) {
+    observerUp.unobserve(lastBox);
+    observerDown.observe(lastBox);
+  }
+
+  // Logic when scrolling UP
+  if (change < 0) {
+    observerDown.unobserve(lastBox);
+    observerUp.observe(lastBox);
+  }
+
+  // set the previous value to current value to compare again
   previous = present;
+});
+
+// function for scroll DOWN
+const scrollDown = function (ObserverEntry) {
+  const entry = ObserverEntry[0];
+
+  // prevent unnecessary calls
+  if (!entry.isIntersecting) return;
+
+  // remove observer from current last item
+  observerDown.unobserve(lastBox);
+
+  // insert the new box
+  boxContainer.insertAdjacentHTML("beforeend", box);
+};
+
+// observer for scroll DOWN
+const observerDown = new IntersectionObserver(scrollDown, {
+  threshold: 1,
+  // rootMargin: `-30px`,
+});
+
+// function for scroll UP
+const scrollUp = function (ObserverEntry) {
+  const entry = ObserverEntry[0];
+
+  // prevent unnecessary calls
+  if (entry.isIntersecting) return;
+
+  // remove observer from current last item
+  observerDown.unobserve(lastBox);
+
+  // remove the last box after moving it to the left
+  if (lastBox.classList.contains("box--moving-right")) {
+    lastBox.classList.toggle("box--moving-right");
+    lastBox.classList.toggle("box--moving-left");
+
+    setTimeout(() => boxContainer.removeChild(lastBox), 100);
+  }
+};
+
+// observer for scroll UP
+const observerUp = new IntersectionObserver(scrollUp, {
+  threshold: 0.5,
 });
