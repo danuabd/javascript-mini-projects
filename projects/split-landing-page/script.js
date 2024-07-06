63d7fe87bc6fbd899f8c9120e4c13447d25f9ad8
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2024-07-06 17:18:01 +0530

    Complete "Split Landing Page" project

--- projects/split-landing-page/script.js
+++ projects/split-landing-page/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// get the container
+const container = document.querySelector(".landing-page");
+
+// get two parts
+const parts = document.querySelectorAll(".page__part");
+
+container.addEventListener("mouseover", function (e) {
+  const target = e.target;
+
+  if (!target.closest(".landing-page")) {
+    parts.forEach((part) => part.classList.remove("grow"));
+    return;
+  }
+
+  parts.forEach((part) => part.classList.remove("grow"));
+
+  target.closest("DIV").classList.add("grow");
+});
+
+container.addEventListener("mouseleave", function (e) {
+  parts.forEach((part) => part.classList.remove("grow"));
+});
