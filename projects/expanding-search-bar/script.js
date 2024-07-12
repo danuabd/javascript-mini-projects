dbf0ecab806ee88fc8045736f4604d043ffa6f1b
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2024-07-12 13:03:26 +0530

    Add global intro style to "Expanding Search Bar"

--- projects/expanding-search-bar/script.js
+++ projects/expanding-search-bar/script.js
@@ -4,15 +4,24 @@
 const searchForm = document.querySelector(".search");
 const searchBar = document.querySelector("#search__bar");
 const searchBtn = document.querySelector(".search__btn");
+let searchBarState = 0;
 
 // close/reveal search bar
-function revealCloseSearchBar(key = 0) {
+function revealCloseSearchBar() {
   // return if user has typed something in search bar
   if (searchBar.value) return;
 
+  if (searchBarState) {
+    searchBar.classList.remove("search__bar--expanded");
+    searchBar.classList.add("search__bar--collapsed");
+    searchBarState = 0;
+  } else {
+    searchBar.classList.add("search__bar--expanded");
+    searchBar.classList.remove("search__bar--collapsed");
+    searchBarState = 1;
+  }
+
   // toggle search bar classes
-  searchBar.classList.toggle("search__bar--expanded");
-  searchBar.classList.toggle("search__bar--collapsed");
 
   // toggle search button class to add left border and radius
   searchBtn.classList.toggle("search__btn--collapsed");
@@ -32,7 +41,7 @@ searchForm.addEventListener("click", function (e) {
   revealCloseSearchBar();
 });
 
-// event handler for document body
+// // event handler for document body
 document.body.addEventListener("click", function (e) {
   if (e.target.closest(".search")) return;
 
